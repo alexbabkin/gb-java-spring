6d576caa1f92a6d197a2ebd43d999ac83b21e5fa
babkin.a <babkin.a@tecomgroup.ru>
2022-06-16 13:04:08 +0300

    hw for lesson-8

--- lesson-7/springdata/src/main/java/com/github/alexbabkin/springdata/services/ProductService.java
+++ lesson-7/springdata/src/main/java/com/github/alexbabkin/springdata/services/ProductService.java
@@ -2,9 +2,10 @@ package com.github.alexbabkin.springdata.services;
 
 import com.github.alexbabkin.springdata.models.Product;
 import com.github.alexbabkin.springdata.repositories.ProductRepository;
-import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,16 +25,20 @@ public class ProductService {
         return productRepository.save(product);
     }
 
-    public List<Product> getProducts(Integer min, Integer max) {
-        if (min == null && max == null) {
-            return productRepository.findAll();
-        }
-        if (min == null) {
-            return productRepository.findProductsByPriceLessThanEqual(max);
-        } else if (max == null) {
-            return productRepository.findProductsByPriceGreaterThanEqual(min);
-        } else {
-            return productRepository.findProductBetweenPrices(min, max);
-        }
+    // public List<Product> getProducts(Integer min, Integer max) {
+    //    if (min == null && max == null) {
+    //        return productRepository.findAll();
+    //    }
+    //    if (min == null) {
+    //        return productRepository.findProductsByPriceLessThanEqual(max);
+    //    } else if (max == null) {
+    //        return productRepository.findProductsByPriceGreaterThanEqual(min);
+    //    } else {
+    //        return productRepository.findProductBetweenPrices(min, max);
+    //    }
+    // }
+
+    public Page<Product> getProducts(int pageIndex, int pageSize) {
+        return productRepository.findAll(PageRequest.of(pageIndex, pageSize));
     }
 }
